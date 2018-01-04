784cfc7e62322d9cf16f784b710305ed6c4fcb49
it89 <ivan.tikhonov.spb@gmail.com>
2018-01-04 19:51:24 +0300

    Add Spring MVC

---
+++ source/investor-daybook/investor-daybook-web/src/main/java/com/github/it89/investordaybook/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.github.it89.investordaybook.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.github.it89.investordaybook")
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+}
