6b91510d3f1c4dd13271db613c6fae981cf8747d
it89 <ivan.tikhonov.spb@gmail.com>
2018-01-08 13:29:05 +0300

    Add Security

---
+++ source/investor-daybook/investor-daybook-web/src/main/java/com/github/it89/investordaybook/model/daybook/Security.java
@@ -0,0 +1,76 @@
+package com.github.it89.investordaybook.model.daybook;
+
+import com.github.it89.investordaybook.model.AppUser;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class Security {
+    @Nullable
+    protected long id;
+    protected String isin;
+    protected SecurityType type;
+    protected String ticker;
+    protected String caption;
+    @Nullable
+    protected String codeGRN;
+    protected AppUser appUser;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIsin() {
+        return isin;
+    }
+
+    public void setIsin(String isin) {
+        if ((isin == null) || isin.length() == 0) {
+            throw new IllegalArgumentException("isin must be specified");
+        }
+        this.isin = isin;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        if ((ticker == null) || ticker.length() == 0) {
+            throw new IllegalArgumentException("ticker must be specified");
+        }
+        this.ticker = ticker;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        if ((caption == null) || caption.length() == 0) {
+            throw new IllegalArgumentException("caption must be specified");
+        }
+        this.caption = caption;
+    }
+
+    public String getCodeGRN() {
+        return codeGRN;
+    }
+
+    public void setCodeGRN(String codeGRN) {
+        this.codeGRN = codeGRN;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        if (appUser == null) {
+            throw new IllegalArgumentException("appUser must be specified");
+        }
+        this.appUser = appUser;
+    }
+}
