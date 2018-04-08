1df572ed07e6af0c5dec3a2f7939302a41aa4610
it89 <ivan.tikhonov.spb@gmail.com>
2018-04-08 21:42:11 +0300

    add CashFlow to SecurityStage

--- source/investor-daybook/investor-daybook-web/src/main/java/com/github/it89/investordaybook/analytics/SecurityStage.java
+++ source/investor-daybook/investor-daybook-web/src/main/java/com/github/it89/investordaybook/analytics/SecurityStage.java
@@ -1,17 +1,23 @@
 package com.github.it89.investordaybook.analytics;
 
 import com.github.it89.investordaybook.model.daybook.Security;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecurityStage {
     private Security security;
     private int stage;
     private LocalDate dateFrom;
+    @Nullable
     private LocalDate dateTo;
+    private List<CashFlow> cashFlow = new ArrayList<>();
+    private List<CashFlow> cashFlowFeature = new ArrayList<>();
 
-    public SecurityStage(Security security, int stage, LocalDate dateFrom, LocalDate dateTo) {
+    public SecurityStage(Security security, int stage, LocalDate dateFrom, @Nullable LocalDate dateTo) {
         this.security = security;
         this.stage = stage;
         this.dateFrom = dateFrom;
@@ -19,10 +25,31 @@ public class SecurityStage {
     }
 
     public SecurityStage(Security security, int stage, LocalDateTime dateFrom, LocalDateTime dateTo) {
-        this.security = security;
-        this.stage = stage;
-        this.dateFrom = dateFrom.toLocalDate();
-        this.dateTo = dateTo.toLocalDate();
+        this(security, stage, dateFrom.toLocalDate(), dateTo.toLocalDate());
+    }
+
+    public Security getSecurity() {
+        return security;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+
+    public List<CashFlow> getCashFlow() {
+        return cashFlow;
+    }
+
+    public List<CashFlow> getCashFlowFeature() {
+        return cashFlowFeature;
     }
 
     @Override
